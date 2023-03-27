c54ec8b64cc48ff3b017a00bd248b0d16dbf3f89
Rajnish714 <srajnish714@gmail.com>
2023-03-27 21:47:42 +0530

    mongo atlas added

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI)
 
 const userSchema = new mongoose.Schema({
     email: String,
